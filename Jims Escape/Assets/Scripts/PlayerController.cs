377a49dfd5b292c2e17afccb3de9e4c7260d803c
Phigy44 <31017193+Phigy44@users.noreply.github.com>
2017-08-16 00:36:42 +0200

    Added Scripts and Testsprites
    Added PlayerController
    Added CameraController
    Added Testsprites

---
+++ Jims Escape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+
+    void Update()
+    {
+
+        //Listening for Inputs
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+    }
+
+}
+
+
+
+
