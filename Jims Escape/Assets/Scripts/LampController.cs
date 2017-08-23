5e640f8bcaafbcbd7f7fdbb2dfccca42e1735059
Phigy44 <31017193+Phigy44@users.noreply.github.com>
2017-08-23 17:54:34 +0200

    Added LampController.cs Script Created Material Folder

---
+++ Jims Escape/Assets/Scripts/LampController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampController : MonoBehaviour {
+
+    public float OilAmout = 500f;
+	
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		
+	}
+}
