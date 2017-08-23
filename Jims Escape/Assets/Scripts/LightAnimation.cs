3e84d2c04b8131bcd7e108192af1660100c967d4
Phigy44 <31017193+Phigy44@users.noreply.github.com>
2017-08-23 18:10:41 +0200

    Added Light Animation Script

---
+++ Jims Escape/Assets/Scripts/LightAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LightAnimation : MonoBehaviour
+{
+
+    Light spotlight;
+    public bool isGrowing;
+
+    void Start()
+    {
+        spotlight = GetComponent<Light>();
+    }
+
+
+    void Update()
+    {
+        //TODO Change the SpotAngel
+        
+
+    }
+}
