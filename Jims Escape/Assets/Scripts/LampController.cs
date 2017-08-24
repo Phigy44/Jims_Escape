17c6fbfa9d41a7a7d664d0b79f9055f4f28edf9d
Phigy44 <31017193+Phigy44@users.noreply.github.com>
2017-08-24 14:11:08 +0200

    Added LampOil Slider Added  LampController Script

--- Jims Escape/Assets/Scripts/LampController.cs
+++ Jims Escape/Assets/Scripts/LampController.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LampController : MonoBehaviour {
+    public Slider oilSlider;
+    public int OilAmout = 100;
 
-    public float OilAmout = 500f;
-	
-	void Start () 
+    public float timeLeft;
+    void Start () 
 	{
-		
-	}
+        timeLeft = OilAmout;
+    }
 	
 	
 	void Update () 
 	{
-		
+        oilSlider.value = OilAmout;
+        
+        timeLeft -= Time.deltaTime ;
+        OilAmout = (int)timeLeft;
+        if (timeLeft < 0) {
+            Debug.Log("No Time Left");
+        }
 	}
 }
