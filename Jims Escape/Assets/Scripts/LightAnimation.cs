0bd4abf9a53b6c27072424d539cb25dee087b8b4
picturestone <lukas.bildstein@outlook.com>
2017-08-24 16:14:36 +0200

    Added "wind" to the light animation
    Looks better now. Good values:
    Min Passes To Rerandomize: 2
    Max Passes To Rerandomize: 6
    Min Amount Of Swing: 1
    Max Amount Of Swing: 3
    Min Windspeed: 1
    Max Windspeed: 4

--- Jims Escape/Assets/Scripts/LightAnimation.cs
+++ Jims Escape/Assets/Scripts/LightAnimation.cs
@@ -12,49 +12,62 @@ public class LightAnimation : MonoBehaviour
     float lastAngleValue = 0f;
     System.Random rnd = new System.Random();
 
+    //number of zero-passes the sine goes through before re-randomizing values
+    int currentNumOfZeroPasses = 0;
+    int maxNumOfZeroPasses = 0;
+    public int minPassesToRerandomizing = 0;
+    public int maxPassesToRerandomizing = 0;
+
     //amountOfSwing is a multiplication of the sine value. use this value to adjust the amount of rising and lowering the angle. should be randomised.
     float amountOfSwing = 0f;
-    public int maxAmountOfSwing = 0;
     public int minAmountOfSwing = 0;
-
+    public int maxAmountOfSwing = 0;
+    
     //speedOfChange stretches the sine curve on the x-axis. use this value to determine how fast the light should change. should be randomised.
-    float speedOfChange = 0f;
-    public int maxSpeedOfChange = 0;
-    public int minSpeedOfChange = 0;
+    //this values act like wind on the lamp.
+    float speedOfAngleChange = 0f;
+    public int minWindspeed = 0;
+    public int maxWindspeed = 0;
 
     //averageAngle is added to the sine curve. it therefor will swing around this value. should be determined by the amount of fuel to feed the lamp.
-    public float averageAngle = 100f;
+    //TODO connect average Angle with the amount of fuel left in the lamp. delete averageAngle = spotlight.spotAngle from start-method
+    float averageAngle = 100f;
 
     void Start()
     {
         spotlight = GetComponent<Light>();
-        randomizeSine();
+        averageAngle = spotlight.spotAngle;
+        randomize();
     }
 
     void Update()
     {
-        //TODO Change the SpotAngle
-
         lastAngleValue = spotlight.spotAngle;
-        //spotlight.spotAngle = (amountOfSwing * Mathf.Sin(speedOfChange * (ticker)) + averageAngle);
-        spotlight.spotAngle = (amountOfSwing * Mathf.Sin(ticker) + averageAngle);
-        Debug.Log("angle = " + spotlight.spotAngle + " | swing = " + amountOfSwing + " | speed " + speedOfChange);
+        spotlight.spotAngle = (amountOfSwing * Mathf.Sin(speedOfAngleChange * (ticker)) + averageAngle);
 
         //tick up timer
         ticker += Time.deltaTime;
 
-        if (Math.Round(spotlight.spotAngle) == averageAngle)
+        //last value is higher than the value the sine swings around and current value is lower --> sine passed from higher to lower half --> randomize, resest timer
+        //last value is lower than the value the sine swings around and current value is higher --> sine passed from lower to higher half --> randomize, reset timer
+        if ((lastAngleValue > averageAngle && spotlight.spotAngle < averageAngle) || (lastAngleValue < averageAngle && spotlight.spotAngle > averageAngle))
         {
-            randomizeSine();
-         //   ticker = 0;
+            currentNumOfZeroPasses++;
+            if(currentNumOfZeroPasses >= maxNumOfZeroPasses)
+            {
+                currentNumOfZeroPasses = 0;
+                ticker = 0;
+                randomize();
+            }
         }
-
-       //TODO fix for different speeds. maybe use multiple random sine-curves added together and change their params after a random time
     }
 
-    void randomizeSine()
+    void randomize()
     {
         amountOfSwing = rnd.Next(minAmountOfSwing, maxAmountOfSwing + 1);
-        speedOfChange = rnd.Next(minSpeedOfChange, maxSpeedOfChange + 1);
+        speedOfAngleChange = rnd.Next(minWindspeed, maxWindspeed + 1);
+        maxNumOfZeroPasses = rnd.Next(minPassesToRerandomizing, maxPassesToRerandomizing + 1);
+        //subtract "windspeed" from nr of passes. this causes medium to high wind to act on the lamp for a very short time only
+        maxNumOfZeroPasses = (int)Math.Round(maxNumOfZeroPasses - speedOfAngleChange);
     }
 }
