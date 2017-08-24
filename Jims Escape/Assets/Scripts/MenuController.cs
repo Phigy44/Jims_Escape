2d9e3e04adb995719cb4af66d69ed3702cdabe41
Phigy44 <31017193+Phigy44@users.noreply.github.com>
2017-08-24 13:53:07 +0200

    Added Loadbar to Menu

--- Jims Escape/Assets/Scripts/MenuController.cs
+++ Jims Escape/Assets/Scripts/MenuController.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
 
-    // Use this for initialization
-    void Start()
+    public void StartGame(int sceneIndex)
     {
+        StartCoroutine(LoadAsync(sceneIndex));
+        loadingScreen.SetActive(true);
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    IEnumerator LoadAsync(int sceneIndex) {
+        AsyncOperation operaiton = SceneManager.LoadSceneAsync(sceneIndex);
+        while (operaiton.isDone == false)
+        {
+            float progress = Mathf.Clamp01(operaiton.progress /.09f);
+            Debug.Log(progress);
+            slider.value = progress;
+            progressText.text = progress.ToString("R");
+            yield return null;
+        }
 
-    }
-
-    public void StartGame()
-    {
-        Application.LoadLevel("MainScene");
     }
 
 }
